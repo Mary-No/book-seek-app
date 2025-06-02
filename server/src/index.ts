c0476271e8addd367025e994b60ab54aa6cb4a1e
Mary-No <mascha.novik@gmail.com>
2025-06-02 14:52:35 +0200

    change front link

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ export const localeMap: Record<Locale, LocaleDefinition> = { en, ru, de };
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://mary-no.github.io/book-seek-app/',
+        'https://Mary-No.github.io/book-seek-app',
     ],
     credentials: true,
 }));
